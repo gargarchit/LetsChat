a4e4ea66257af8204f7ff30df8c36ae24687e810
Archit <garg4.ag@gmail.com>
2018-12-23 10:56:55 +0000

    Addded MongoDb database

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/Letschat");
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
-var post = [
-        {caption: "Hello Everyone", image: "https://images.pexels.com/photos/237018/pexels-photo-237018.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
-        ];
+
+var postSchema = new mongoose.Schema({
+   caption: String,
+   image: String
+});
+
+var post = mongoose.model('post', postSchema);
 
 app.get('/', (req,res) => {
     res.render('index');
 });
 
 app.get('/post', (req,res) => {
-    
-    res.render("index", {post:post});
+    post.find( {}, (err,post) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {post:post});
+        }
+    });
 });
 
 app.post('/post', (req,res) => {
     var caption = req.body.caption;
     var image = req.body.image;
     var newpost = {caption:caption,image:image};
-    post.push(newpost);
-    res.redirect('/post');
+    post.create(newpost, (err,post) => {
+            if(err){
+                console.log(err);
+            } else {
+                res.redirect('/post');
+            }
+    });
 });
 
 app.get('/createpost', (req,res) => {
